5ea46333cfe5446e8c64fab2f9d9970152767f04
Ralph Zapata <ralph_zapata@yahoo.com>
2020-04-30 09:46:03 +0930

    changing format

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/* Bootstrap, jQuery & Popper.JS */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
